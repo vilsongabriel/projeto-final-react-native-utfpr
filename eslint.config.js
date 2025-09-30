fdc31aa65f20116810c8a6918ee8b73ca7a2e70a
Vilson Onofre <vilson@onofre.dev>
2025-09-30 20:56:15 -0300

    configura lint e prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,16 @@
 // https://docs.expo.dev/guides/using-eslint/
-const { defineConfig } = require('eslint/config');
-const expoConfig = require('eslint-config-expo/flat');
+const { defineConfig } = require('eslint/config')
+const expoConfig = require('eslint-config-expo/flat')
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended')
 
 module.exports = defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
-]);
+	expoConfig,
+	eslintPluginPrettierRecommended,
+	{
+		ignores: ['dist/*'],
+		rules: {
+			semi: ['error', 'never'],
+			'@typescript-eslint/no-explicit-any': 'error',
+		},
+	},
+])
